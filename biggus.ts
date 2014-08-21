364deac9ea7aac599a0ce41a71b611e56fb17137
Drew Noakes <git@drewnoakes.com>
2014-08-21 15:01:08 +0100

    Reorder items in file.

--- biggus.ts
+++ biggus.ts
@@ -410,6 +410,131 @@ export enum SortDirection
     Descending
 }
 
+export interface INotifyChange<T>
+{
+    subscribeChange(callback: (item:T)=>void): ()=>void;
+    notifyChange(): void;
+}
+
+class NotifyChange
+{
+    private callbacks: {(item:any):void}[];
+
+    public subscribeChange(callback: (item:any)=>void): ()=>void
+    {
+        if (!this.callbacks)
+            this.callbacks = [];
+        this.callbacks.push(callback);
+        return () =>
+        {
+            var index = this.callbacks.indexOf(callback);
+            if (index === -1)
+                console.warn("Attempt to unsubscribe unknown subscriber");
+            else
+                this.callbacks.splice(index, 1);
+        };
+    }
+
+    public notifyChange(): void
+    {
+        if (this.callbacks)
+        {
+            for (var i = 0; i < this.callbacks.length; i++)
+                this.callbacks[i](this);
+        }
+    }
+}
+
+var notifyChangePrototype = Object.create(NotifyChange.prototype);
+
+export function mixinNotifyChange(obj: any)
+{
+    obj.__proto__ = notifyChangePrototype;
+}
+
+enum CollectionChangeType
+{
+    Insert,
+    Update,
+    Remove,
+    Move
+}
+
+class CollectionChange<T>
+{
+    public type: CollectionChangeType;
+    public item: T;
+    public newIndex: number;
+    public oldIndex: number;
+
+    public static insert<U>(item: U, index: number): CollectionChange<U>
+    {
+        var chg = new CollectionChange<U>();
+        chg.type = CollectionChangeType.Insert;
+        chg.item = item;
+        chg.newIndex = index;
+        chg.oldIndex = -1;
+        return chg;
+    }
+
+    public static remove<U>(item: U, index: number): CollectionChange<U>
+    {
+        var chg = new CollectionChange<U>();
+        chg.type = CollectionChangeType.Remove;
+        chg.item = item;
+        chg.newIndex = -1;
+        chg.oldIndex = index;
+        return chg;
+    }
+
+    public static update<U>(item: U, index: number): CollectionChange<U>
+    {
+        var chg = new CollectionChange<U>();
+        chg.type = CollectionChangeType.Update;
+        chg.item = item;
+        chg.newIndex = index;
+        chg.oldIndex = -1;
+        return chg;
+    }
+
+    public static move<U>(item: U, newIndex: number, oldIndex: number): CollectionChange<U>
+    {
+        var chg = new CollectionChange<U>();
+        chg.type = CollectionChangeType.Move;
+        chg.item = item;
+        chg.newIndex = newIndex;
+        chg.oldIndex = oldIndex;
+        return chg;
+    }
+}
+
+interface IObservableCollection<T>
+{
+    changed: Event<CollectionChange<T>>;
+}
+
+/**
+ * A basic, observable, append-only data source.
+ */
+class DataSource<T> implements IObservableCollection<T>
+{
+    public changed: Event<CollectionChange<T>>;
+
+    private items: T[] = [];
+
+    public add(item: T)
+    {
+        this.items.push(item);
+        var change: CollectionChange<T> = CollectionChange.insert(item, this.items.length - 1);
+        this.changed.raise(change);
+    }
+
+    public get(index: number)
+    {
+        return this.items[index];
+    }
+}
+
 export class Grid<TRow>
 {
     private thead: HTMLTableSectionElement;
@@ -591,131 +716,5 @@ export class Grid<TRow>
 
         for (var i = 0; i < models.length; i++)
             this.tbody.appendChild(models[i].tr);
-    }
-}
-
-
-export interface INotifyChange<T>
-{
-    subscribeChange(callback: (item:T)=>void): ()=>void;
-    notifyChange(): void;
-}
-
-class NotifyChange
-{
-    private callbacks: {(item:any):void}[];
-
-    public subscribeChange(callback: (item:any)=>void): ()=>void
-    {
-        if (!this.callbacks)
-            this.callbacks = [];
-        this.callbacks.push(callback);
-        return () =>
-        {
-            var index = this.callbacks.indexOf(callback);
-            if (index === -1)
-                console.warn("Attempt to unsubscribe unknown subscriber");
-            else
-                this.callbacks.splice(index, 1);
-        };
-    }
-
-    public notifyChange(): void
-    {
-        if (this.callbacks)
-        {
-            for (var i = 0; i < this.callbacks.length; i++)
-                this.callbacks[i](this);
-        }
-    }
-}
-
-var notifyChangePrototype = Object.create(NotifyChange.prototype);
-
-export function mixinNotifyChange(obj: any)
-{
-    obj.__proto__ = notifyChangePrototype;
-}
-
-enum CollectionChangeType
-{
-    Insert,
-    Update,
-    Remove,
-    Move
-}
-
-class CollectionChange<T>
-{
-    public type: CollectionChangeType;
-    public item: T;
-    public newIndex: number;
-    public oldIndex: number;
-
-    public static insert<U>(item: U, index: number): CollectionChange<U>
-    {
-        var chg = new CollectionChange<U>();
-        chg.type = CollectionChangeType.Insert;
-        chg.item = item;
-        chg.newIndex = index;
-        chg.oldIndex = -1;
-        return chg;
-    }
-
-    public static remove<U>(item: U, index: number): CollectionChange<U>
-    {
-        var chg = new CollectionChange<U>();
-        chg.type = CollectionChangeType.Remove;
-        chg.item = item;
-        chg.newIndex = -1;
-        chg.oldIndex = index;
-        return chg;
-    }
-
-    public static update<U>(item: U, index: number): CollectionChange<U>
-    {
-        var chg = new CollectionChange<U>();
-        chg.type = CollectionChangeType.Update;
-        chg.item = item;
-        chg.newIndex = index;
-        chg.oldIndex = -1;
-        return chg;
-    }
-
-    public static move<U>(item: U, newIndex: number, oldIndex: number): CollectionChange<U>
-    {
-        var chg = new CollectionChange<U>();
-        chg.type = CollectionChangeType.Move;
-        chg.item = item;
-        chg.newIndex = newIndex;
-        chg.oldIndex = oldIndex;
-        return chg;
-    }
-}
-
-interface IObservableCollection<T>
-{
-    changed: Event<CollectionChange<T>>;
-}
-
-/**
- * A basic, observable, append-only data source.
- */
-class DataSource<T> implements IObservableCollection<T>
-{
-    public changed: Event<CollectionChange<T>>;
-
-    private items: T[] = [];
-
-    public add(item: T)
-    {
-        this.items.push(item);
-        var change: CollectionChange<T> = CollectionChange.insert(item, this.items.length - 1);
-        this.changed.raise(change);
-    }
-
-    public get(index: number)
-    {
-        return this.items[index];
     }
 }
