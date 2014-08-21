7d40fbd89d7090cc95a1d6fff599238f0b38a8bd
Drew Noakes <git@drewnoakes.com>
2014-08-21 12:09:16 +0100

    Add TODO.

--- biggus.ts
+++ biggus.ts
@@ -502,6 +502,7 @@ export class Grid<TRow>
             this.bindRow(rowModel);
 
             // Flash the row that changed
+            // TODO only queue this timer if the row is visible (once we model this)
             tr.classList.add('highlight-delta');
             setTimeout(function() { tr.classList.remove('highlight-delta'); }, 100);
         }
